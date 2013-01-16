e382918c2f3ba3ba2f1a668ba30422cb1a5b0cf8
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2013-01-16 08:25:06 +0000

    take more params
    git-svn-id: http://svn.sj.9x9.tv:8080/repos/misc/9x9dev/trunk/nnqueue@4234 a0a5f996-2bb4-401c-8778-395c3c01700f

--- src/main/java/com/nncloudtv/queue/Task.java
+++ src/main/java/com/nncloudtv/queue/Task.java
@@ -6,6 +6,7 @@ import java.io.ObjectInputStream;
 import java.util.logging.Logger;
 
 import com.nncloudtv.lib.NnNetUtil;
+import com.nncloudtv.lib.QueueFactory;
 
 public class Task {
 
@@ -26,18 +27,39 @@ public class Task {
 	    return obj;
 	}	
 	
+//    obj[0] = msgurl;
+//    obj[1] = method;
+//    obj[2] = contentType;
+//    obj[3] = data;
+
 	public static void doWork(byte[] message) {		
         Object[] obj = (Object[])Task.toObject(message);        
-        String url = (String) obj[0];        
+        String url = (String) obj[0];
+        String method = (String) obj[1];
+        String contentType = (String) obj[2];
+        String data = (String) obj[3];
         log.info("[x] url:" + url);
         Object json = (Object) obj[1];
+        if (method.equals(QueueFactory.METHOD_GET)) {
+            NnNetUtil.urlGet(url);
+        } else {        
+            if (contentType.equals(QueueFactory.CONTENTTYPE_JSON)) {
+                log.info("[x] json:" + json);
+                NnNetUtil.urlPostWithJson(url, json);            
+            } else {
+                log.info("url:" + url);
+                log.info("data:" + data);
+                NnNetUtil.urlPost(url, data);
+            }
+        }
+        /*
         if (json == null) {
         	NnNetUtil.urlGet(url);
         } else {
         	log.info("[x] json:" + json);
     		NnNetUtil.urlPostWithJson(url, json);
         }
-		
+		*/
 	    log.info("[x] Done" );
 	}
 	
