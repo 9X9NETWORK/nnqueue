d97685b959455eb5a5899debd38cc514514746c7
yiwen <yiwen@a0a5f996-2bb4-401c-8778-395c3c01700f>
2013-01-17 00:58:48 +0000

    git-svn-id: http://svn.sj.9x9.tv:8080/repos/misc/9x9dev/trunk/nnqueue@4242 a0a5f996-2bb4-401c-8778-395c3c01700f

---
+++ installer/jobs/purge.py
@@ -0,0 +1,20 @@
+# install python packagesudo apt-get install python-amqplib
+# generate queue list
+#  1.sudo rabbitmqctl list_queues -p / name > queues.txt
+#  2.keep only queue names: remove the first line, last line and number after queue names
+
+from amqplib import client_0_8 as amqp
+
+conn = amqp.Connection(host="127.0.0.1:5672", userid="guest", password="guest", virtual_host="/", insist=False)
+conn = conn.channel()
+
+queues = None
+with open('queues.txt', 'r') as f:
+    queues = f.readlines()
+
+for q in queues:
+    if q:
+        #print 'deleting %s' % q
+        conn.queue_purge(q.strip())
+
+print 'purged %d items' % len(queues)
